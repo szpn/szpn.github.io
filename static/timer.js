cffee175a5f9514ec9fd2a7eaacf61bc5283b2fd
jan <szopengry@gmail.com>
2018-07-25 18:31:23 +0200

    added google analysis

--- static/timer.js
+++ static/timer.js
@@ -68,7 +68,6 @@ let toggleButtons = [];
 let resetButtons = [];
 let respawnTimes = [7 * 60 + 30, 12 * 60 + 30, 26 * 60 + 30, 46 * 60 + 30, undefined, 10 * 60, 20 * 60, 30 * 60, 49 * 60 + 30];
 let bossNames = ["Mylfid", "Olimpus", "Veryhtus", "Temani", undefined, "Magiczny Metin", "Metin Lodu", "Metin Spustoszenia", "Metin Asherod"];
-//let bossImages = [];
 let allowNotifiaiotns = false;
 
 for (let i = 0; i < $(".boss").length; i++) {
